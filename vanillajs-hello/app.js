2519e0530a3817710b0626442da31ae28d818e00
AzaleaMoon10 <carla.clemente.010@gmail.com>
2025-10-29 13:04:46 +0100

    Excuse generator with user input
    Added user input functionality to the excuse generator.

--- vanillajs-hello/app.js
+++ vanillajs-hello/app.js
@@ -1,19 +1,31 @@
 function randomElement(a) {
+  if(a.length == 0){
+    return "";
+  }
   return a[Math.floor(Math.random() * a.length)];
 }
 
 function excuseGen() {
-  return randomElement(who).concat(" ", randomElement(action), " ", randomElement(what), " ", randomElement(when));
+  return randomElement(who).concat(" ", randomElement(action), " ", randomElement(what), " ", randomElement(when), " ",randomElement(user));
 }
 
 function buttonFunc() {
-  document.getElementById("excuse").innerHTML = `<h1>${excuseGen(who, action, what, when)}</h1>`;
+  document.getElementById("excuse").innerHTML = `<h1>${excuseGen(who, action, what, when, user)}</h1>`;
+}
+
+function inputArray() {
+  user.push(document.getElementById("inputText").value);
+  document.getElementById("inputText").value = "";
 }
 
 const who = ['The dog', 'My grandma', 'The mailman', 'My bird'];
 const action = ['ate', 'peed', 'crushed', 'broke'];
 const what = ['my homework', 'my phone', 'the car'];
 const when = ['before the class', 'when I was sleeping', 'while I was exercising', 'during my lunch', 'while I was praying'];
+let user = [];
+
+document.getElementById("buttonSubmit").addEventListener("click", inputArray);
+document.getElementById("buttonGen").addEventListener("click", buttonFunc);
 
 window.onload = function() {
   //write your code here
@@ -22,6 +34,7 @@ window.onload = function() {
 
   document.getElementById("excuse").innerHTML = `<h1>${excuse}</h1>`;
 }
+
 
 
 
